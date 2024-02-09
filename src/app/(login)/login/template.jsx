c9462b688cd59ef907ebe9cd98ce4e8197e904f9
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-02-10 02:06:39 +0530

    another test

--- src/app/(login)/login/template.jsx
+++ src/app/(login)/login/template.jsx
@@ -8,6 +8,7 @@ function template({ children }) {
       <motion.div
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
+        exit={{opacity: 0, x: -100}}
         transition={{
           delay: 0.6,
           duration: 1,
