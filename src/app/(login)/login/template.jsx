66c71961be47f0977b7b91ac0f5489fe8b36271e
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-02-10 02:10:55 +0530

    another test

--- src/app/(login)/login/template.jsx
+++ src/app/(login)/login/template.jsx
@@ -1,24 +1,19 @@
 "use client";
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function template({ children }) {
+function Template({ children }) {
+    const [timer, setTimer] = useState(false)
+    useEffect(() => {
+        setTimeout(() => {
+            setTimer(true)
+        }, (1000));
+    })
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        exit={{opacity: 0, x: -100}}
-        transition={{
-          delay: 0.6,
-          duration: 1,
-          type: "tween",
-          ease: "anticipate",
-        }}
-      >test</motion.div>
-      {children}
+      {timer && children}
     </>
   );
 }
 
-export default template;
+export default Template;
