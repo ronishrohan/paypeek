39a948175fae5438276ce36514c4acffcd2b514c
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-02-11 01:04:08 +0530

    fixed loading animation

--- src/app/(main)/components/sidebar/SidebarLink.jsx
+++ src/app/(main)/components/sidebar/SidebarLink.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { icons } from "@/app/utils/icons";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import {motion} from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion";
 
 function SidebarLink({ icon, title, children, click, ...rest }) {
   const pathname = usePathname().substring(1);
@@ -25,8 +25,8 @@ function SidebarLink({ icon, title, children, click, ...rest }) {
     }
   }, [loading]);
   useEffect(() => {
-    console.log(timer)
-  }, [timer])
+    console.log(timer);
+  }, [timer]);
   return (
     <Link
       href={`/${icon}`}
@@ -39,13 +39,23 @@ function SidebarLink({ icon, title, children, click, ...rest }) {
       id={`${icon === pathname ? "active" : "inactive"}`}
       {...rest}
     >
-      <div id="sidebar-button-icon">
-        {loading && timer>100 ? (
-          <motion.span initial={{opacity: 0, scale: 0}} animate={{opacity: 1, scale:1}} id="sidebar-loading-icon">{icons["loading"]}</motion.span>
-        ) : (
-          icons[icon]
-        )}
-      </div>
+      <AnimatePresence>
+        <div id="sidebar-button-icon">
+          {loading && timer > 100 ? (
+            <motion.span
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{opacity: 0}}
+              transition={{type:"tween"}}
+              id="sidebar-loading-icon"
+            >
+              {icons["loading"]}
+            </motion.span>
+          ) : (
+            icons[icon]
+          )}
+        </div>
+      </AnimatePresence>
       <div id="sidebar-button-text">{title}</div>
 
       {children}
