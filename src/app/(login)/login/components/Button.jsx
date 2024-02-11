e987e1d1cc8ece73af33c9aeec41d3b5f8a7de16
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-02-11 15:37:04 +0530

    added loading animations for all buttons

--- src/app/(login)/login/components/Button.jsx
+++ src/app/(login)/login/components/Button.jsx
@@ -1,29 +1,39 @@
-"use client"
+"use client";
 import { motion, AnimatePresence } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
 import { icons } from "@/app/utils/icons";
-import {signIn} from "next-auth/react"
+import { signIn } from "next-auth/react";
 
-function Button({children}) {
+function Button({ children }) {
+  let [loading, setLoading] = useState(false);
   return (
     <motion.button
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y : 0 }}
-        transition={{
-          delay: 0.2 + (Math.random()),
-          duration: 1,
-          type: "tween",
-          ease: "anticipate",
-        }}
-        onClick={() => signIn(children.toLowerCase(), {callbackUrl: "/home"})}
-        id="login-button"
-      >
-        <div>
-          <span id="login-google">{children}</span>
-        </div>
-        <div id="login-icon">{icons[children.toLowerCase()]}</div>
-      </motion.button>
-  )
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.2 + Math.random(),
+        duration: 1,
+        type: "tween",
+        ease: "anticipate",
+      }}
+      onClick={() => {
+        setLoading(true)
+        signIn(children.toLowerCase(), { callbackUrl: "/home" });
+      }}
+      id="login-button"
+    >
+      {loading ? (
+        <span id="sidebar-loading-icon">{icons.loading}</span>
+      ) : (
+        <>
+          <div>
+            <span id="login-google">{children}</span>
+          </div>
+          <div id="login-icon">{icons[children.toLowerCase()]}</div>
+        </>
+      )}
+    </motion.button>
+  );
 }
 
-export default Button
+export default Button;
--- src/app/(main)/components/sidebar/Logout.jsx
+++ src/app/(main)/components/sidebar/Logout.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { icons } from "@/app/utils/icons";
 import { signOut } from "next-auth/react";
 
 function Logout() {
+  let [loading, setLoading] = useState(false);
   return (
-    <button onClick={() => signOut({callbackUrl: "/login"})} id="profile-logout" title="logout :(" >
-      {icons.logout}
+    <button
+      onClick={() => {
+        setLoading(true);
+        signOut({ callbackUrl: "/login" });
+      }}
+      id="profile-logout"
+      title="logout :("
+    >
+      {loading ? (
+        <span id="sidebar-loading-icon">{icons.loading}</span>
+      ) : (
+        icons.logout
+      )}
     </button>
   );
 }
